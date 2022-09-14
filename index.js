5c996ac4ed8dd37c3249d281f707402dd1ebddfc
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-09-14 16:02:17 -0500

    built the basic game logic function for the project

--- index.js
+++ index.js
@@ -10,13 +10,41 @@ const array = ['Rock', 'Paper', 'Scissors'];
 
 const computerSelection = getComputerChoice(array);
 
-console.log(computerSelection);
-
-playerSelection = 'sciSSORS';
+playerSelection = 'Rock';
 
 let firstLetter = playerSelection.slice(0, 1);
 let otherLetters = playerSelection.slice(1);
-let playerChoice = firstLetter.toUpperCase(firstLetter) + otherLetters.toLowerCase(otherLetters);
+let playerChoice = firstLetter.toUpperCase(firstLetter) + 
+otherLetters.toLowerCase(otherLetters);
+
+playerSelection = playerChoice;
+
+console.log('player choice = ' + playerSelection);
+console.log('computer choice = ' +computerSelection);
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+        return 'Tie';
+    }  else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You win! Rock beats Scissors.';
+    } else if (playerSelection === "Rock" && computerSelection === 'Paper') {
+        return 'You lose! Paper beats Rock.';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
+        return 'Tie';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You win! Paper beats Rock.';
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You lose! Scissors beat Paper.';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        return 'Tie';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Papper') {
+        return 'You win! Scissors beat Paper.';
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You lose! Rock beats Scissors';
+    }
+   
+   
+}
 
 
 
@@ -24,4 +52,4 @@ let playerChoice = firstLetter.toUpperCase(firstLetter) + otherLetters.toLowerCa
 
 
 
-console.log(playerChoice);
+console.log(playRound(playerSelection,computerSelection));
