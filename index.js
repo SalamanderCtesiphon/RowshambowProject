da3472c44c98ca2561fd0cf1153c17ae8ab14aa9
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-09-20 13:46:00 -0500

    formalized the functions to generate computer choice in order to invoke it during the main game function

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 // generate a random choice from the computer
-Array.prototype.random = function () {
-    return this[Math.floor((Math.random())*this.length)]
+const options = ["Rock", "Paper", "Scissors"];
+function computerChoice (options) {
+    const computerSelection = Math.floor((Math.random()) * options.length);
+    return options[computerSelection];
 }
-var list = ['Rock', 'Paper', 'Scissors'];
-const computerSelection = list.random();
 // take an input from the player
-const playerChoice = (prompt("Please enter your selection for the game you: [Rock, Paper or Scissors}"));
+const playerChoice ="";
+function playerInput (playerChoice) {
+    playerChoice = (prompt("Please enter your selection for the game: [Rock, Paper or Scissors}"));
+    return playerChoice;
+}
+let middleStep = playerInput(playerChoice);
 // format the input for comparison
-function capitalize (playerChoice) {
-    let first = playerChoice.slice(0,1);
-    let rest = playerChoice.slice(1);
+function capitalize (middleStep) {
+    let first = middleStep.slice(0,1);
+    let rest = middleStep.slice(1);
     let newName = first.toUpperCase(first) + rest.toLowerCase(rest);
     return newName;
 }
-let playerSelection = capitalize(playerChoice);
+let playerSelection = capitalize(middleStep);
 //check to make sure that the computer and player selctions are 
 // working properly
 
 console.log('Player: ' + playerSelection);
         
-console.log('Computer: ' + computerSelection);
+console.log('Computer: ' + computerChoice(options));
 
 // the function to give the results of the game for each round
+let computerSelection = "";
 
 function playRound(playerSelection, computerSelection) {
+    computerChoice(options);
     if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
         return 'You win! Rock beats Scissors';
     } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
