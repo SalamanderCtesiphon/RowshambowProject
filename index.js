adfe76531a9b013e2de2297d1ad488f84a15e216
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-09-19 22:23:03 -0500

    made some alterations

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ Array.prototype.random = function () {
     return this[Math.floor((Math.random())*this.length)]
 }
 var list = ['Rock', 'Paper', 'Scissors'];
-let computerSelection = list.random();
+
 // take an input from the player
-let playerChoice = (prompt("Please enter your selection for the game you: [Rock, Paper or Scissors}"));
+const playerChoice = (prompt("Please enter your selection for the game you: [Rock, Paper or Scissors}"));
 // format the input for comparison
 function capitalize (playerChoice) {
     let first = playerChoice.slice(0,1);
@@ -13,7 +13,7 @@ function capitalize (playerChoice) {
     let newName = first.toUpperCase(first) + rest.toLowerCase(rest);
     return newName;
 }
-let playerSelection = capitalize(playerChoice);
+
 //check to make sure that the computer and player selctions are 
 // working properly
 
@@ -23,9 +23,11 @@ console.log('Computer: ' + computerSelection);
 
 // the function to give the results of the game for each round
 
-function playRound(playerSelection, computerSelection) {
+function playRound() {
+    const computerSelection = list.random();
+    let playerSelection = capitalize(playerChoice);
     if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        return 'You win! Rock beats Scissor';
+        return 'You win! Rock beats Scissors';
     } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
         return 'You lose! Paper beats Rock';
     } else if ( playerSelection == 'Rock' && computerSelection == 'ROck') {
@@ -44,14 +46,24 @@ function playRound(playerSelection, computerSelection) {
         return 'Tie';
     } 
 }
+// trying to envoke the game five times and create a tally of wins for
+// each player
+let playerWins = 0;
+let computerWins = 0;
 let results = playRound(playerSelection, computerSelection);
-
 console.log(results);
 
-
-function game() {
+function game(playerWins, computerWins) {
     for (let i = 0; i < 5; i++) {
-
+        if (results == 'You win! Rock beats Scissors' || results == 'You win! Paper beats Rock' || results == 'You win! Scissors beat Paper') {
+            playerWins++;            
+        } else if (results == 'You lose! Rock beats Scissors' || results == 'You lose! Paper beats Rock' || results == 'You lose! Scissors beat Paper') {
+            computerWins++;            
+        }
+        return computerWins, playerWins;
     }
-
 }
+
+game(playerWins, computerWins);
+console.log(playerWins);
+console.log(computerWins)
